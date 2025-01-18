2e596b9e3bc7a43be084a871e1e4bdf80c6b7ec6
Manuele D'Ambrosio <manuele.dambrosio@studio.unibo.it>
2025-01-18 16:42:01 +0100

    Started login page (index page)

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
+    require_once 'bootstrap.php';
 
+    //Params
+    $params["title"] = "Ear-Reader Login";
+    $params["main"] = "login.php";
+
+    
+    require 'template/base.php';
 ?>
